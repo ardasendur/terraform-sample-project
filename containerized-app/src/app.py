b5c5d5d8ffd08168e7a2550422dd424bf45e53fb
Arda Sendur <ardasendur@Arda-MacBook-Pro.local>
2021-06-22 23:51:07 +0300

    json return type

--- containerized-app/src/app.py
+++ containerized-app/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 import math
 
 app = Flask(__name__)
@@ -6,17 +6,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return jsonify(
+        result=str("Happy Flask App"),
+        status=200
+    )
+
 
 @app.route("/factorial/<number>")
 def factorial(number):
     if int(number) == 0:
         result: int = 1
     elif int(number) < 0:
-        result = "No valid input!Please run with positive number..."
+        result = "No valid input"
     else:
         result = math.factorial(int(number))
-    return str("Factorial of " + str(number) + " is : " + str(result))
+    return jsonify(
+        result=str(result),
+        status=200
+    )
 
 
 if __name__ == "__main__":
