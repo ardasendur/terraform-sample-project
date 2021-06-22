d31053456b111bde7538f4cd9a96277521912938
Arda Sendur <ardasendur@Arda-MacBook-Pro.local>
2021-06-22 23:00:44 +0300

    refactoed and fix

--- containerized-app/src/app.py
+++ containerized-app/src/app.py
@@ -8,15 +8,16 @@ app = Flask(__name__)
 def index():
     return "Hello World!"
 
-
 @app.route("/factorial/<number>")
 def factorial(number):
-    if int(number) <= 0:
+    if int(number) == 0:
         result: int = 1
+    elif int(number) < 0:
+        result = "No valid input!Please run with positive number..."
     else:
         result = math.factorial(int(number))
-    return str(result)
+    return str("Factorial of " + str(number) + " is : " + str(result))
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
--- serverless-app/terraform/aws/app.tfvars
+++ serverless-app/terraform/aws/app.tfvars
@@ -1,6 +1,3 @@
 bucket_name= "my-s3-bucket-for-assignment"
-
-
 function_name = "csv-loader"
-
 dynamodb_name = "my-dynamodb-for-assignment"
